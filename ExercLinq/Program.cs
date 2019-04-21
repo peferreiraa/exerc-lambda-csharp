2892a354aa719494036ebddea9b56de6965724bb
Pietro Ferreira <piiiietro.pereira@hotmail.com>
2019-04-21 18:49:56 -0300

    Add project files.

---
+++ ExercLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
